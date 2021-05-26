787fa22327ae480d625d746786d87e5e480a81cb
Jeremy DUFOSSE <jdufosse@eto.fr>
2021-05-26 11:38:03 +0200

    Adds components + Bulma-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { MainComponent } from './main/main.component';
+import { ThematicComponent } from './thematic/thematic.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CompetenceComponent } from './competence/competence.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    MainComponent
+    MainComponent,
+    ThematicComponent,
+    ExperienceComponent,
+    CompetenceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/thematic/thematic.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faCoffee, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+@Component({
+  selector: 'app-thematic',
+  templateUrl: './thematic.component.html',
+  styleUrls: ['./thematic.component.scss']
+})
+export class ThematicComponent implements OnInit {
+  public faCoffee = faCoffee;
+  public faStar = faStar;
+  public faStarHalfAlt = faStarHalfAlt;
+  public farStar = farStar;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  get model(): any { return this._model; }
+  set model(model: any) {
+    this._model = model;
+  }
+  private _model = null;
+
+  @Input()
+  get index(): number { return this._index; }
+  set index(index: number) {
+    this._index = index;
+  }
+  private _index = 0;
+}
