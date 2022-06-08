b5f031c3978277656bf19fa8a2f66592ae0670d7
Okkari <okkari.user@gmailcom>
2022-06-08 23:57:39 +0200

    feat(prismic): Add prismic + modify header to use model

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import * as model from 'src/app/types/types';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  public data: any = {};
+export class HeaderComponent implements OnInit, OnDestroy {
+  public data: model.General = undefined;
+  private _handleGeneralChangeCallback : (data: model.General) => void;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {
+    this._handleGeneralChangeCallback = this.handleGeneralChange.bind(this)
+  }
+
+  ngOnDestroy(): void {
+    this.dataService.unsubscribeGeneralChange(this._handleGeneralChangeCallback);
+  }
 
   ngOnInit(): void {
+    this.dataService.subscribeGeneralChange(this._handleGeneralChangeCallback);
     this.data = this.dataService.getGeneral();
+
+    console.log('HeaderComponent-ngOnInit', { data: this.data });
+  }
+
+  private handleGeneralChange(data : model.General): void {
+    if (data) {
+      this.data = data;
+      console.log('HeaderComponent-handleGeneralChange', { data: this.data });
+    }
   }
 }
